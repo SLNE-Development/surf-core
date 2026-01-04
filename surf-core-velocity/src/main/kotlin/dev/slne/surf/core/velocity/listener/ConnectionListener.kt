304674c102178f4f6be448b2d177c953e917b10d
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-01-04 18:04:40 +0100

    feat: enhance ConnectionListener to handle additional player events
    - Add handling for `PlayerChooseInitialServerEvent` to manage initial server connections.
    - Add handling for `KickedFromServerEvent` to manage player kicks and trigger disconnect logic.
    - Improve connection logging with detailed server transition and disconnection messages.

--- surf-core-velocity/src/main/kotlin/dev/slne/surf/core/velocity/listener/ConnectionListener.kt
+++ surf-core-velocity/src/main/kotlin/dev/slne/surf/core/velocity/listener/ConnectionListener.kt
@@ -3,6 +3,8 @@ package dev.slne.surf.core.velocity.listener
 import com.github.shynixn.mccoroutine.velocity.launch
 import com.velocitypowered.api.event.Subscribe
 import com.velocitypowered.api.event.connection.DisconnectEvent
+import com.velocitypowered.api.event.player.KickedFromServerEvent
+import com.velocitypowered.api.event.player.PlayerChooseInitialServerEvent
 import com.velocitypowered.api.event.player.ServerPostConnectEvent
 import dev.slne.surf.core.api.common.event.SurfPlayerConnectEvent
 import dev.slne.surf.core.api.common.event.SurfPlayerDisconnectEvent
@@ -12,19 +14,10 @@ import dev.slne.surf.core.velocity.plugin
 import kotlin.jvm.optionals.getOrNull
 
 object ConnectionListener {
-    @Subscribe
-    fun onConnected(event: ServerPostConnectEvent) {
-        val previousServer = event.previousServer
-        val newServer = event.player.currentServer.getOrNull()?.serverInfo?.name
-
-        if (previousServer != null) {
-            val player =
-                surfPlayerService.players.find { it.uuid == event.player.uniqueId } ?: return
-            player.currentServer = newServer
-
-            surfPlayerService.cachePlayer(player)
-            return
-        }
+    @Subscribe(priority = Short.MIN_VALUE)
+    fun onLogin(event: PlayerChooseInitialServerEvent) {
+        val newServer = event.initialServer.getOrNull()?.serverInfo?.name
+        println("[connection: new] ${event.player.username} (${event.player.remoteAddress}) connected to '$newServer'")
 
         plugin.pluginContainer.launch {
             val player = surfPlayerService.getOrLoadOrCreatePlayerByUuid(
@@ -51,6 +44,49 @@ object ConnectionListener {
     }
 
     @Subscribe
+    fun onConnected(event: ServerPostConnectEvent) {
+        val previousServer = event.previousServer
+        val newServer = event.player.currentServer.getOrNull()?.serverInfo?.name
+
+        if (previousServer != null) {
+            val player =
+                surfPlayerService.players.find { it.uuid == event.player.uniqueId } ?: return
+            player.currentServer = newServer
+
+            println("[connection: update] ${event.player.username} was redirected from '${previousServer.serverInfo.name}' to '$newServer'")
+
+            surfPlayerService.cachePlayer(player)
+        }
+    }
+
+    @Subscribe
+    fun onKick(event: KickedFromServerEvent) {
+        when (event.result) {
+            is KickedFromServerEvent.DisconnectPlayer -> {
+                val player = surfPlayerService.findPlayerByUuid(event.player.uniqueId) ?: return
+
+                surfEventBus.fire(
+                    SurfPlayerDisconnectEvent(
+                        player
+                    )
+                )
+
+                surfPlayerService.invalidatePlayer(player.uuid)
+
+                println("[connection: closed] ${event.player.username} disconnected")
+
+                plugin.pluginContainer.launch {
+                    surfPlayerService.savePlayer(player.apply {
+                        lastSeen = System.currentTimeMillis()
+                    })
+                }
+            }
+
+            else -> {}
+        }
+    }
+
+    @Subscribe
     fun onDisconnect(event: DisconnectEvent) {
         val player = surfPlayerService.findPlayerByUuid(event.player.uniqueId) ?: return
 
@@ -61,6 +97,8 @@ object ConnectionListener {
         )
 
         surfPlayerService.invalidatePlayer(player.uuid)
+
+        println("[connection: closed] ${event.player.username} disconnected")
 
         plugin.pluginContainer.launch {
             surfPlayerService.savePlayer(player.apply {

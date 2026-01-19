be41c19e1c69560c335cb919ad530490fb21ef01
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-01-19 14:35:24 +0100

    refactor: replace `SurfEventHandler` import with `OnRedisEvent` in `VelocitySurf

--- surf-core-velocity/src/main/kotlin/dev/slne/surf/core/velocity/redis/listener/VelocitySurfPlayerRedisListener.kt
+++ surf-core-velocity/src/main/kotlin/dev/slne/surf/core/velocity/redis/listener/VelocitySurfPlayerRedisListener.kt
@@ -1,8 +1,8 @@
 package dev.slne.surf.core.velocity.redis.listener
 
-import dev.slne.surf.core.api.common.event.SurfEventHandler
 import dev.slne.surf.core.core.common.redis.event.SurfPlayerMessageRedisEvent
 import dev.slne.surf.core.velocity.plugin
+import dev.slne.surf.redis.event.OnRedisEvent
 import kotlin.jvm.optionals.getOrNull
 
 object VelocitySurfPlayerRedisListener {

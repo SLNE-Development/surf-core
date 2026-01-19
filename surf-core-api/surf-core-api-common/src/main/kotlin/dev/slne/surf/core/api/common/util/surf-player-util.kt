b33f7b89731810ea253451401d0946eb503995d0
TheBjoRedCraft <143264463+TheBjoRedCraft@users.noreply.github.com>
2026-01-19 14:12:06 +0100

    Update surf-core-api/surf-core-api-common/src/main/kotlin/dev/slne/surf/core/api/common/util/surf-player-util.kt
    Co-authored-by: twisti <76837088+twisti-dev@users.noreply.github.com>

--- surf-core-api/surf-core-api-common/src/main/kotlin/dev/slne/surf/core/api/common/util/surf-player-util.kt
+++ surf-core-api/surf-core-api-common/src/main/kotlin/dev/slne/surf/core/api/common/util/surf-player-util.kt
@@ -5,5 +5,5 @@ import dev.slne.surf.core.api.common.surfCoreApi
 import dev.slne.surf.surfapi.core.api.messages.builder.SurfComponentBuilder
 
 inline fun SurfPlayer.sendText(text: SurfComponentBuilder.() -> Unit) {
-    surfCoreApi.sendText(this, SurfComponentBuilder().apply(text).build())
+    surfCoreApi.sendText(this, SurfComponentBuilder(text))
 }

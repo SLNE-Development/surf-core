e3f19aba18379d9b15bc7a28e465efc16b1633d6
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-01-25 21:04:17 +0100

    feat: add `/nserver` command for network server teleportation
    - Implemented `networkServerCommand` to allow players to teleport between servers using the `/nserver` command.
    - Added `COMMAND_NETWORK_SERVER` permission to `PermissionRegistry`.

--- surf-core-paper/src/main/kotlin/dev/slne/surf/core/paper/permission/PermissionRegistry.kt
+++ surf-core-paper/src/main/kotlin/dev/slne/surf/core/paper/permission/PermissionRegistry.kt
@@ -10,6 +10,7 @@ object PermissionRegistry : PermissionRegistry() {
     val COMMAND_NETWORK_LIST = create("$BASE_COMMAND.networklist")
     val COMMAND_NETWORK_TELEPORT = create("$BASE_COMMAND.networkteleport")
     val COMMAND_WHERE_AM_I = create("$BASE_COMMAND.whereami")
+    val COMMAND_NETWORK_SERVER = create("$BASE_COMMAND.networkserver")
 
     val COMMAND_INFO = create("$BASE_COMMAND.info")
     val COMMAND_INFO_PLAYER = create("$BASE_COMMAND.info.player")

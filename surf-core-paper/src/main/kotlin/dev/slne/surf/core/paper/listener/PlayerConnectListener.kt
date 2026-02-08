2fca73e4f0416edd4b92905015bb07a8ccc87e72
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-02-08 01:18:37 +0100

    feat: rename PlayerClientLoadedListener to PlayerConnectListener and add player connection handling

--- surf-core-paper/src/main/kotlin/dev/slne/surf/core/paper/listener/PlayerConnectListener.kt
+++ surf-core-paper/src/main/kotlin/dev/slne/surf/core/paper/listener/PlayerConnectListener.kt
@@ -1,6 +1,6 @@
 package dev.slne.surf.core.paper.listener
 
-import dev.slne.surf.core.api.paper.util.surfPlayer
+import dev.slne.surf.core.core.common.player.surfPlayerService
 import dev.slne.surf.core.core.common.util.formatMillis
 import dev.slne.surf.core.paper.permission.PermissionRegistry
 import dev.slne.surf.surfapi.core.api.messages.adventure.buildText
@@ -9,12 +9,19 @@ import dev.slne.surf.surfapi.core.api.messages.adventure.sendText
 import io.papermc.paper.event.player.PlayerClientLoadedWorldEvent
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent
+import org.bukkit.event.player.PlayerKickEvent
 
-object PlayerClientLoadedListener : Listener {
+object PlayerConnectListener : Listener {
     @EventHandler
     fun onPlayerClientLoaded(event: PlayerClientLoadedWorldEvent) {
         val player = event.player
-        val surfPlayer = event.surfPlayer
+        val surfPlayer = surfPlayerService.findPlayerByUuid(player.uniqueId)
+
+        if (surfPlayer == null) {
+            player.kick(buildDisconnectComponent(), PlayerKickEvent.Cause.UNKNOWN)
+            return
+        }
 
         if (!event.player.hasPermission(PermissionRegistry.JOIN_WHERE_AM_I)) {
             return
@@ -41,5 +48,35 @@ object PlayerClientLoadedListener : Listener {
             variableValue(surfPlayer.currentProxy?.name ?: "Unbekannt")
             info(".")
         }
+    }
+
+    @EventHandler
+    fun onPlayerConnect(event: AsyncPlayerPreLoginEvent) {
+        val surfPlayer = surfPlayerService.findPlayerByUuid(event.uniqueId)
+
+        if (surfPlayer == null) {
+            event.disallow(AsyncPlayerPreLoginEvent.Result.KICK_OTHER, buildDisconnectComponent())
+        }
+    }
+
+    private fun buildDisconnectComponent() = buildText {
+        appendNewline()
+        primary("CASTCRAFTER")
+        appendNewline()
+        primary("COMMUNITY SERVER")
+        appendNewline()
+        appendNewline()
+        error("DEINE DATEN KONNTEN NICHT GELADEN WERDEN.")
+        appendNewline()
+        error("Code: 1921186215185: 1") // surf-core in A1Z26-Cipher + Error Code 1
+        appendNewline()
+        appendNewline()
+        appendNewline()
+        spacer("Beim laden deiner Daten ist ein interner Fehler aufgetreten.")
+        appendNewline()
+        spacer("Sollte das Problem weiterhin bestehen, wende dich bitte an den Support.")
+        appendNewline()
+        appendNewline()
+        primary("discord.gg/castcrafter")
     }
 }

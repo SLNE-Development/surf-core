51841dffde4ffa10c71d6dbbed80689f66a0cac2
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-02-08 02:12:09 +0100

    feat: update error logging to use dynamic error code in PlayerConnectListener

--- surf-core-paper/src/main/kotlin/dev/slne/surf/core/paper/listener/PlayerConnectListener.kt
+++ surf-core-paper/src/main/kotlin/dev/slne/surf/core/paper/listener/PlayerConnectListener.kt
@@ -80,7 +80,7 @@ object PlayerConnectListener : Listener {
         appendNewline(2)
         error("DEINE SPIELERDATEN KONNTEN NICHT GELADEN WERDEN.")
         appendNewline()
-        error("Code: 1921186215185: 1") // surf-core in A1Z26-Cipher + Error Code 1
+        error("Code: $code")
         appendNewline(3)
         spacer("Beim laden deiner Spielerdaten ist ein interner Fehler aufgetreten.")
         appendNewline()

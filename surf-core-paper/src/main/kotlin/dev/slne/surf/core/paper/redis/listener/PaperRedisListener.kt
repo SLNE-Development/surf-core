133ac2d3afcaf30fbb0dc649bbe11be12cab7eab
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-02-12 16:49:28 +0100

    fix: update max players handling in PaperRedisListener to include server service update

--- surf-core-paper/src/main/kotlin/dev/slne/surf/core/paper/redis/listener/PaperRedisListener.kt
+++ surf-core-paper/src/main/kotlin/dev/slne/surf/core/paper/redis/listener/PaperRedisListener.kt
@@ -2,6 +2,7 @@ package dev.slne.surf.core.paper.redis.listener
 
 import dev.slne.surf.core.api.common.server.SurfServer
 import dev.slne.surf.core.core.common.redis.event.SurfServerChangeMaxPlayersRedisEvent
+import dev.slne.surf.core.core.common.server.surfServerService
 import dev.slne.surf.redis.event.OnRedisEvent
 import org.bukkit.Bukkit
 
@@ -10,6 +11,12 @@ object PaperRedisListener {
     fun onMaxPlayerChange(event: SurfServerChangeMaxPlayersRedisEvent) {
         if (event.server.name == SurfServer.current().name) {
             Bukkit.setMaxPlayers(event.maxPlayers)
+
+            surfServerService.addServer(
+                SurfServer.current().copy(
+                    maxPlayers = event.maxPlayers
+                )
+            )
         }
     }
 }

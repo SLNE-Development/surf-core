4241cd3b831cd4ba0f7945d2bf1003253a07e8df
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-03-15 01:24:47 +0100

    feat: update logging level for invalid player synchronization

--- surf-core-velocity/src/main/kotlin/dev/slne/surf/core/velocity/task/SurfPlayerSyncTask.kt
+++ surf-core-velocity/src/main/kotlin/dev/slne/surf/core/velocity/task/SurfPlayerSyncTask.kt
@@ -47,12 +47,10 @@ class SurfPlayerSyncTask {
                 .filter { it.uuid !in onlineUuids }
                 .forEach {
                     surfPlayerService.invalidatePlayer(it.uuid)
-                    plugin.logger.warn("Found invalid player ${it.uuid} (${it.username}), invalidating cache")
+                    plugin.logger.info("Found invalid player ${it.uuid} (${it.username}), invalidating cache")
                 }
 
             onlinePlayers.forEach { surfPlayerService.cachePlayer(it) }
-
-            plugin.logger.info("Synchronized ${onlinePlayers.size} online players")
         }
     }
 }

4422f059d552464d0ff6e2d28b65235654fe81ae
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-04-05 00:13:49 +0200

    chore: update surf-api-gradle-plugin and settings version to 26.1+

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
         maven("https://repo.slne.dev/repository/maven-public/") { name = "maven-public" }
     }
     dependencies {
-        classpath("dev.slne.surf:surf-api-gradle-plugin:26+")
+        classpath("dev.slne.surf:surf-api-gradle-plugin:26.1+")
         classpath("dev.slne.surf.microservice:surf-microservice-gradle-plugin:1.21.11+")
     }
 }

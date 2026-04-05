bdd9e0158d93fca8d645ffc819b1d5658fec4dc6
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-04-05 21:12:15 +0200

    chore: update surf-microservice-gradle-plugin version to latest

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,11 +4,10 @@ buildscript {
     repositories {
         gradlePluginPortal()
         maven("https://reposilite.slne.dev/releases")
-        maven("https://repo.slne.dev/repository/maven-public/") { name = "maven-public" }
     }
     dependencies {
         classpath("dev.slne.surf.api:surf-api-gradle-plugin:+")
-        classpath("dev.slne.surf.microservice:surf-microservice-gradle-plugin:1.21.11+")
+        classpath("dev.slne.surf.microservice:surf-microservice-gradle-plugin:+")
     }
 }
 

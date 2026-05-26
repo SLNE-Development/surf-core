67a39722f10d324fe0402f331ce383dfdc83ccde
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-05-26 02:05:00 +0200

    feat: add surfCoreApiCommon dependency to build configuration

--- surf-core-launcher/surf-core-launcher-server/build.gradle.kts
+++ surf-core-launcher/surf-core-launcher-server/build.gradle.kts
@@ -6,6 +6,7 @@ dependencies {
     api(projects.surfCoreLauncher.surfCoreLauncherApi)
     implementation("dev.slne.surf.redis:surf-redis-api:1.6.0")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.2")
+    implementation(projects.surfCoreApi.surfCoreApiCommon)
 }
 
 tasks.shadowJar {

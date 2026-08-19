444d3faa5092bcf7d4a3c3dd67a0ff54e2079b99
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-08-19 15:21:41 +0200

    feat: implement player resource management with local and remote cache, database query and minecraft session server request

--- surf-core-core/surf-core-core-client/src/main/kotlin/dev/slne/surf/core/client/player/resource/PlayerResourceCache.kt
+++ surf-core-core/surf-core-core-client/src/main/kotlin/dev/slne/surf/core/client/player/resource/PlayerResourceCache.kt
@@ -2,11 +2,13 @@ package dev.slne.surf.core.client.player.resource
 
 import com.github.benmanes.caffeine.cache.Caffeine
 import com.sksamuel.aedile.core.asLoadingCache
+import com.sksamuel.aedile.core.expireAfterWrite
 import dev.slne.surf.api.core.serializer.java.uuid.SerializableUUID
 import dev.slne.surf.core.api.common.resource.PlayerResource
 import dev.slne.surf.core.client.ClientCoreInstance
 import dev.slne.surf.core.core.common.resource.PlayerResourceService
 import java.util.*
+import kotlin.time.Duration.Companion.days
 
 object PlayerResourceCache {
     private val rpc by lazy {
@@ -14,14 +16,16 @@ object PlayerResourceCache {
     }
 
     private val textureCacheByUuid =
-        Caffeine.newBuilder().asLoadingCache<UUID, PlayerResource?> { uuid ->
-            rpc.findPlayerResourceByUuid(uuid)
-        }
+        Caffeine.newBuilder().expireAfterWrite(1.days)
+            .asLoadingCache<UUID, PlayerResource?> { uuid ->
+                rpc.findPlayerResourceByUuid(uuid)
+            }
 
     private val textureCacheByName =
-        Caffeine.newBuilder().asLoadingCache<String, PlayerResource?> { username ->
-            rpc.findPlayerResourceByName(username)
-        }
+        Caffeine.newBuilder().expireAfterWrite(1.days)
+            .asLoadingCache<String, PlayerResource?> { username ->
+                rpc.findPlayerResourceByName(username)
+            }
 
     suspend fun resourceFromUuid(playerUuid: SerializableUUID) =
         textureCacheByUuid.get(playerUuid)
--- surf-core-microservice/src/main/kotlin/dev/slne/surf/core/microservice/resource/MicroservicePlayerResourceCache.kt
+++ surf-core-microservice/src/main/kotlin/dev/slne/surf/core/microservice/resource/MicroservicePlayerResourceCache.kt
@@ -2,24 +2,28 @@ package dev.slne.surf.core.microservice.resource
 
 import com.github.benmanes.caffeine.cache.Caffeine
 import com.sksamuel.aedile.core.asLoadingCache
+import com.sksamuel.aedile.core.expireAfterWrite
 import dev.slne.surf.api.core.serializer.java.uuid.SerializableUUID
 import dev.slne.surf.core.api.common.resource.PlayerResource
 import dev.slne.surf.core.microservice.resource.database.PlayerResourceRepository
 import dev.slne.surf.core.microservice.resource.source.sources.MinecraftPlayerResourceSource
 import java.util.*
+import kotlin.time.Duration.Companion.days
 
 object MicroservicePlayerResourceCache {
     private val textureCacheByUuid =
-        Caffeine.newBuilder().asLoadingCache<UUID, PlayerResource?> { uuid ->
-            PlayerResourceRepository.findPlayerResource(uuid)
-                ?: MinecraftPlayerResourceSource.findPlayerResource(uuid)
-        }
+        Caffeine.newBuilder().expireAfterWrite(1.days)
+            .asLoadingCache<UUID, PlayerResource?> { uuid ->
+                PlayerResourceRepository.findPlayerResource(uuid)
+                    ?: MinecraftPlayerResourceSource.findPlayerResource(uuid)
+            }
 
     private val textureCacheByName =
-        Caffeine.newBuilder().asLoadingCache<String, PlayerResource?> { username ->
-            PlayerResourceRepository.findPlayerResource(username)
-                ?: MinecraftPlayerResourceSource.findPlayerResource(username)
-        }
+        Caffeine.newBuilder().expireAfterWrite(1.days)
+            .asLoadingCache<String, PlayerResource?> { username ->
+                PlayerResourceRepository.findPlayerResource(username)
+                    ?: MinecraftPlayerResourceSource.findPlayerResource(username)
+            }
 
     suspend fun findPlayerResource(playerUuid: SerializableUUID) =
         textureCacheByUuid.get(playerUuid)
